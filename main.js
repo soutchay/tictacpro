571fe8a0ccb97c3ef2d580833988420099ef1de1
Test <test@Tests-MacBook-Pro.local>
2014-10-09 00:15:51 -0700

    fixed animation with JS only

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-var boxapp = angular.module("boxApp", []);
+// var boxapp = angular.module("boxApp", []);
 
-boxapp.controller("BoxController", function($scope) {
-	$scope.spinaround = function() {
-		$scope.spinning = "rotatebox";
-	};
-	$scope.normalize = function() {
-		$scope.spinning = "box";
-	};
-});
+// boxapp.controller("BoxController", function($scope) {
+// 	$scope.spinaround = function() {
+// 		$scope.spinning = "rotatebox";
+// 	};
+// 	$scope.normalize = function() {
+// 		$scope.spinning = "box";
+// 	};
+// 	$scope.boxArray = [0,1,2,3,4,5,6,7,8];
+	
+// });
