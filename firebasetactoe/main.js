7e78a64583259787760f3a9735476114f3ac40af
Test <test@Tests-MacBook-Pro.local>
2014-10-20 04:58:38 -0700

    added comments, removed some code for current version

--- firebasetactoe/main.js
+++ firebasetactoe/main.js
@@ -1,19 +1,10 @@
-// with angular we can do stuff that curl does
-// var myHttp;
-//declare variable myHttp
-
-
 var app = angular.module("myApp", ["firebase"]);
 
 app.controller("BoxController", ["$scope", "$firebase", function ($scope, $firebase){
-	// console.log('shit works');
-	// console.log($scope.rateme);
+
  	$scope.remoteGameContainer = $firebase(new Firebase("https://tactictoe.firebaseio.com/databaseGameContainer")) ;
- 	var something = new Firebase("https://tactictoe.firebaseio.com/databaseGameContainer");
-	// var restartArray = 
-	// [{player: 0, placeholder:""}, {player: 0, placeholder:""}, {player: 0, placeholder:""},
-	//  {player: 0, placeholder:""}, {player: 0, placeholder:""}, {player: 0, placeholder:""},
-	//  {player: 0, placeholder:""}, {player: 0, placeholder:""}, {player: 0, placeholder:""}];
+ 	var firebasedata = new Firebase("https://tactictoe.firebaseio.com/databaseGameContainer");
+ 	//different variables for angularfire and firebase respectively
 	$scope.restartArray = new Object;
 		$scope.restartArray["0"] = {player:"A", placeholder: " "};
 		$scope.restartArray["1"] = {player:"A", placeholder: " "};
@@ -24,28 +15,24 @@ app.controller("BoxController", ["$scope", "$firebase", function ($scope, $fireb
 		$scope.restartArray["6"] = {player:"A", placeholder: " "};
 		$scope.restartArray["7"] = {player:"A", placeholder: " "};
 		$scope.restartArray["8"] = {player:"A", placeholder: " "};
-	console.log($scope.restartArray);
-	// $scope.boxArray = angular.copy($scope.restartArray);
-	// //an array used to make the board
-	// // console.log($scope.boxArray);
-	// // $scope.scoreBoard = [{first: }, {second: }]
-	// $scope.turn = 0;
-	// $scope.gameOver = false;
-	$scope.gameScore =  {xScore: 0, oScore: 0, ties: 0};
-	//turn and gameOver defined for turns of the game and when the game is over respectively
+
+	$scope.gameScore =  {xScore: "", oScore: "", ties: ""};
 	$scope.isXThenO = true;
-	//variable to switch between X and O	
+	//variable isXThenO to switch between X and O	
 	$scope.turn = 0;
 	$scope.gameOver = false;
+	//turn and gameOver defined for turns of the game and when the game is over respectively
 	$scope.specialIsOff = true;
+	//specialIsOff variable for when a player can hide their move
 
-	something.once("value", function(data) {
+	firebasedata.once("value", function(data) {
 		if(data.val().totalNumPlayers == 2) {
 			$scope.currentPlayer = 0;
 		}
 		else {
 			$scope.currentPlayer = 1;
 		}
+		//instantiates a local variable that gets referenced to in firebase to make sure one player cannot play when it is not their turn
 		$scope.gameContainer = {
 		  cellListArray: $scope.restartArray,
 		  clickCounter: $scope.turn,
@@ -55,23 +42,20 @@ app.controller("BoxController", ["$scope", "$firebase", function ($scope, $fireb
 		  specialMoveOff: $scope.specialIsOff,
 		  totalNumPlayers: $scope.currentPlayer +1
 		} ;	
+		//gameContainer is an object with properties that have the angular variables as their values in order to store data into firebase
 		$scope.remoteGameContainer.$bind($scope, "gameContainer") ;
-
+		//binds the variable gameContainer to the angularfire database
 		$scope.restartGame();
 	})	
 
 
 	$scope.cellOnClick = function (cellID) {
-		console.log("this is the ", cellID);
-		console.log("clicked " + cellID.player);
-		// console.log("this is", something);
-		// console.log("restart array is", restartArray);
-		if ($scope.gameContainer.isGameOver ==true) {
+		if ($scope.gameContainer.isGameOver == true) {
 			return;
 		}
+		//function will return if the game is over
 		if ($scope.gameContainer.specialMoveOff){
 			if (cellID.player == "A" && $scope.gameContainer.isGameOver == false && $scope.currentPlayer == ($scope.gameContainer.clickCounter % 2) ) {
-				// console.log("true or false"+$scope.gameContainer.clickCounter % 2 == 0);
 				if ($scope.gameContainer.clickCounter % 2 == 0) {
 					cellID.placeholder = $scope.gameContainer.isXFirst ? "X" : "O";
 					//selects the cell clicked on and puts an "X"
@@ -83,13 +67,12 @@ app.controller("BoxController", ["$scope", "$firebase", function ($scope, $fireb
 					cellID.player = "O";
 				}
 				$scope.gameContainer.clickCounter ++;
-				console.log(cellID.player + " this is what it is");
 			}
-			console.log("does this NOT WORK", $scope.gameContainer.specialMoveOff);
 		}
+
+		//else statement if a player used the special move button
 		else {
-			console.log("does this work", $scope.gameContainer.specialMoveOff);
-			if (cellID.player == "A" && $scope.gameContainer.isGameOver == false)
+			if (cellID.player == "A" && $scope.gameContainer.isGameOver == false && $scope.currentPlayer == ($scope.gameContainer.clickCounter % 2))
 				if ($scope.gameContainer.clickCounter % 2 == 0) {
 					//selects the cell clicked on and puts an "X"
 					cellID.placeholder = " ";
@@ -100,14 +83,12 @@ app.controller("BoxController", ["$scope", "$firebase", function ($scope, $fireb
 					cellID.placeholder =  " ";
 					cellID.player = "O";
 				}
-				$scope.gameContainer.clickCounter ++;
-				console.log($scope.gameContainer.specialMoveOff);	
+				$scope.gameContainer.clickCounter ++;	
 				$scope.gameContainer.specialMoveOff = true;	
 		}		
-		//the above is to determine who's turn it is and what gets placed in the property player
-		// console.log(this.$index);
-		// console.log(this.cell.player)
+		//the above is to determine who's turn it is and what gets placed in the player property
 		
+		// conditions to win the game
 		if ($scope.gameContainer.cellListArray["0"].player == "X" && $scope.gameContainer.cellListArray["1"].player == "X" && $scope.gameContainer.cellListArray["2"].player == "X" ||
 			$scope.gameContainer.cellListArray["3"].player == "X" && $scope.gameContainer.cellListArray["4"].player == "X" && $scope.gameContainer.cellListArray["5"].player == "X" ||
 			$scope.gameContainer.cellListArray["6"].player == "X" && $scope.gameContainer.cellListArray["7"].player == "X" && $scope.gameContainer.cellListArray["8"].player == "X" ||
@@ -150,7 +131,6 @@ app.controller("BoxController", ["$scope", "$firebase", function ($scope, $fireb
 			}
 			$scope.gameContainer.isGameOver = true;						 
 		}
-		// conditions to win the game
 		else {
 			console.log("it is turn " + $scope.gameContainer.clickCounter);
 			if ($scope.gameContainer.clickCounter == 9 && $scope.gameContainer.isGameOver == false) {
@@ -160,7 +140,6 @@ app.controller("BoxController", ["$scope", "$firebase", function ($scope, $fireb
 			}
 		}
 		//results in a tie at 9 turns and if the game is not over
-	// console.log($scope.gameScore);
 	};
 	
 	// RESET GAME
@@ -169,146 +148,10 @@ app.controller("BoxController", ["$scope", "$firebase", function ($scope, $fireb
 		$scope.gameContainer.isGameOver = false;
 		$scope.gameContainer.clickCounter = 0;
 		$scope.gameContainer.specialMoveOff = true;
-		console.log("reset game");
-		console.log($scope.gameContainer.cellListArray);
 	};
-	// $scope.restartGame();
+	// Function to hide player's move
 	$scope.special = function() {
-		console.log("works");
 		return $scope.gameContainer.specialMoveOff = false;
 	}
 
 }]);
-
-	// console.log($scope.boxArray[0].player)
-	// for ($scope.i = 0; $scope.i < $scope.boxArray.length; $scope.i++) {
-	// 	if ($scope.boxArray[i].player ==1){
-	// 		if($scope.boxArray[1]==1 && $scope.boxArray[2] ==1) {
-	// 			console.log("player win");
-	// 		}
-	// 	}
-	// }
-	// $scope.cellOnHover = function () {
-	// 	console.log('shit');
-	// 	console.log(angular.srcElement);
-
-	// 	//  if (this.cell.player ==0) {
-	// 	// 		angular.element(e.srcElement).addClass('tHi');
-	// 	// }
-	// };
-// 		    // return function(scope, element) {
-//       //   element.bind('mouseenter', function(){
-//       //       element.addClass('hover');
-//       //   }).bind('mouseleave', function(){
-//       //     element.removeClass('hover');
-//       //   })
-//     }
-// })
-// 	}
-		// if ($scope.boxArray[4].player == 1 &&
-		// 	$scope.boxArray[0].player == 1 || $scope.boxArray[2].player == 1 || $scope.boxArray[6].player==1 || $scope.boxArray[8].player==1 ||
-		// 	$scope.boxArray[3].player ==1 || $scope.boxArray[1].player ==1 || $scope.boxArray[5].player ==1 || $scope.boxArray[3].player ==1 &&
-		// 	$scope.boxArray[]) {
-		// 	alert('win');
-		// };
-//	if middle,
-		// then check if has top left, top right, bottom left, or bottom right, 
-		// 	if top left, then check bottom right
-		// 	if top right then check bottom left	
-		
-		//alternate between player 1 and player 2
-//********
-// app.directive('myRotate', function(){
-// 	return {
-// 		restrict: 'A',
-// 		scope: {
-// 			thatcell: '=mydata'
-// 			//=mean binding goes both ways, so can change it in controller, bowf will be the variable in the scope of the controller
-// 			//the mydata is name attribute in the div that is pointing to the cell in the boxArray in the controller
-// 		},	
-// 	controller: function($scope) {
-// 			console.log('it works! this controller');
-// 			console.log($scope.thatcell);
-// 			console.log($scope.thatcell.player);
-// 			console.log(this);
-// 			// while ($scope.bowf.name)	
-//     //   element.bind("mouseover", function(e){    
-//     //        element.addClass('unfill').removeClass('filled');
-//     //   });
-//     //   element.bind("mouseout", function(e){
-//     //        element.addClass('filled').removeClass('unfill');
-//     //   }),
-//     // link: function ($scope, element, attrs) {     
-//     //   element.bind("mouseover", function(e){    
-//     //        element.addClass('unfill').removeClass('filled');
-//     //   });
-//     //   element.bind("mouseout", function(e){
-//     //        element.addClass('filled').removeClass('unfill');
-//     //   }),						
-// 		}
-// 	}
-// });
-//*********************
-// app.directive('myRotate', function(){
-// 	return {
-// 		restrict: 'A',
-// 		scope: {
-// 			bowf: '=mydata'
-// 			//=mean binding goes both ways, so can change it in controller, bowf will be the variable in the scope of the controller
-// 			//the mydata is name attribute in the div that is pointing to the cell in the boxArray in the controller
-// 		},
-// 	controller: function($scope) {
-// 			console.log('it works! this controller');
-// 			console.log($scope.bowf);
-// 			console.log($scope.bowf.player);
-// 			console.log(this);
-// 			if ($scope.bowf.player == 0) {
-// 			}
-// 			// while ($scope.bowf.name)		
-// 		}
-// 	}
-// });
-	// //code above works, testing out how to create directives 
- 
-    //     element.bind('mouseenter', function(){
-    //         element.addClass('rotatebox');
-    //     }).bind('mouseleave', function(){
-    //       element.removeClass('rotatebox');
-    //     })
-    // }
-// document.getElementById("restart").addEventListener("click", function(){
-// 	window.location.reload();
-// });
-app.directive('ourRating', function () {
-    return {
-      restrict: 'A',
-      template: '<ul class="rating">' +
-                  '<li ng-repeat="star in stars" class="filled">' +
-                      '\u2605' +
-                  '</li>' +
-                '</ul>',
-        //template is what gets put into the div
-      scope: {
-      	// ratingValue: '=ratingValuee'
-      	ratingValue: '=rateme'
-      },      
-
-      link: function (scope, elem, attrs) {
-      	//scope, elem attached to, attrs are other parameters
-        //console.log(scope, "Directive", elem, elem.scope(), "Attributes", attrs.ourRating.split(":"));
-        //separate scope on every element, elem refers to the div, scope is elem.scope(), scope of THE DIV
-        // comma can be used as a space and +
-        console.log(scope.ratingValue);
-        rating = 5;
-        console.log(rating);
-        //in scope, rating value is defined to equal itself in the html or whatever it is referencing to
-        scope.stars = [];
-        for (var i = 0; i < scope.ratingValue; i++) {
-          scope.stars.push({});
-        }
-            
-      }
-        //u2605 is the unicode for a star
-    }
-    //returning an object that contains a function
-});
